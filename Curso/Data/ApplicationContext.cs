40f1121040b670cc1f4c374f890e3114405ea612
phillrog <phillrog@hotmail.com>
2021-05-21 11:01:07 -0300

    Foi configurado o modelcreating

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -14,17 +14,7 @@ namespace CursoEFCore.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>( c =>{
-            c.ToTable("Clientes");
-            c.HasKey(p => p.Id);
-            c.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
-            c.Property(p => p.Telefone).HasColumnType("CHAR(11)");
-            c.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
-            c.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
-            c.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
-            c.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
-        });
-
+           modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
     }
 }
