50ba771e9b3edf10e2b97f19e7e9926f3efe0221
phillrog <phillrog@hotmail.com>
2021-05-21 15:25:08 -0300

    Adicionando multiplos registros

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CursoEFCore.Domain;
 using CursoEFCore.ValueObjects;
@@ -16,9 +17,55 @@ namespace CursoEFCore
            // var existe = db.Database.GetPendingMigrations().Any();
 
            // Console.WriteLine("Hello World!");
-           InserirDados();
+           //InserirDados();
+
+           InserirDadosEmMassa();
         }
 
+private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Rafael Almeida",
+                CEP = "99999000",
+                Cidade = "Itabaiana",
+                Estado = "SE",
+                Telefone = "99000001111",
+            };
+
+            var listaClientes = new List<Cliente>();
+
+            for (int i = 5000; i < 25000; i++)
+            {
+                listaClientes.Add( 
+                new Cliente
+                {
+                    Nome = $"Teste {i}",
+                    CEP = "99999000",
+                    Cidade = "Itabaiana",
+                    Estado = "SE",
+                    Telefone = "99000001115",
+                });
+            }
+
+
+            using var db = new Data.ApplicationContext();
+            //db.AddRange(produto, cliente);
+            db.Set<Cliente>().AddRange(listaClientes);
+            //db.Clientes.AddRange(listaClientes);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+        }
         private static void InserirDados()
         {
             var produto = new Produto
