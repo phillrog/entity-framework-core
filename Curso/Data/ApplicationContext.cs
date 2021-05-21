91ea90208e1b0386252f9e60911665ac1cf0c22f
phillrog <phillrog@hotmail.com>
2021-05-21 10:50:02 -0300

    Mapeamento através do Fluent API

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,13 +1,30 @@
+using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext: DbContext 
     {
+        public DbSet<Pedido> Pedidos {get;set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder); 
             optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>( c =>{
+            c.ToTable("Clientes");
+            c.HasKey(p => p.Id);
+            c.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+            c.Property(p => p.Telefone).HasColumnType("CHAR(11)");
+            c.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
+            c.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
+            c.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
+            c.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
+        });
+
         }
     }
 }
